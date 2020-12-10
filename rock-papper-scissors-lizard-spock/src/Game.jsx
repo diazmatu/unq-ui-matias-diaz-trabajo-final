69ffc21dd1d1e01b86f5cc45d67645be144975ac
MATIASDIAZ\matu_ <matu_spiderman1@hotmail.com>
2020-12-09 23:28:14 -0300

    Ultimos arreglos de css y Logica

--- rock-papper-scissors-lizard-spock/src/Game.jsx
+++ rock-papper-scissors-lizard-spock/src/Game.jsx
@@ -6,11 +6,6 @@ const Game = () => {
     const [rivalScore, setRivalScore] = useState()
     const [playerChoice, setPlayerChoice] = useState()
     const [rivalChoice, setRivalChoice] = useState()
-    const [rock, setRock] = useState()
-    const [paper, setPaper] = useState()
-    const [scissors, setScissors] = useState()
-    const [lizard, setLizard] = useState()
-    const [spock, setSpock] = useState()
     const [matchResult, setMatchResult] = useState()
 
     useEffect(() => {
@@ -18,32 +13,96 @@ const Game = () => {
         setRivalScore(0)
         setPlayerChoice(document.getElementById("playerChoice"))
         setRivalChoice(document.getElementById("rivalChoice"))
-        setRock(document.getElementById("Rock"))
-        setPaper(document.getElementById("Paper"))
-        setScissors(document.getElementById("Scissors"))
-        setLizard(document.getElementById("Lizard"))
-        setSpock(document.getElementById("Spock"))
     }, []);
-
-    const playerLost = () =>{
-        console.log(vs)
-        if(vs === "computer"){setMatchResult("You lost this match")} 
-        else{setMatchResult("The second player win this match")}
-        setRivalScore(rivalScore + 1)
+    
+    const chooseRival = (event) =>{
+        if(event.target.id === "Player"){
+            setVS("player")
+        } else{
+            setVS("computer") 
+        }
+        
+        document.getElementById("vs").hidden = true
+        rematch()
     }
 
-    const playerWin = () =>{
-        console.log("pre")
-        console.log(playerScore)
-        console.log(rivalScore)
-        if(vs === "computer"){setMatchResult("You win this match")} 
-        else{setMatchResult("The first player win this match")}
-        setPlayerScore(playerScore + 1)
-        console.log("post")
-        console.log(playerScore)
-        console.log(rivalScore)
+    const rematch = () =>{        
+        document.getElementById("userPlayer").hidden = false
+        document.getElementById("result").hidden = true
+        document.getElementById("matchOptions").hidden = true
+        document.getElementById("changeRival").hidden = false
     }
 
+    const computer = (event, play) =>{
+        computerPlay()
+        player(event, play)
+        decideWinner(play, rivalChoice)
+    }
+
+    const player = (event, play) => {
+        document.getElementById("userPlayer").hidden = true
+
+        if(document.getElementById("rivalPlayer").hidden && vs === "player"){
+            document.getElementById("rivalPlayer").hidden = false
+        } else{
+            document.getElementById("rivalPlayer").hidden = true
+            document.getElementById("result").hidden = false
+            document.getElementById("matchOptions").hidden = false
+        } 
+
+        setImg(event.target.id, play)
+
+        if(play === rivalChoice){
+            decideWinner(playerChoice, rivalChoice)
+        }
+    }
+
+    const computerPlay = (event) => {
+        const random = Math.floor(Math.random() * 5) + 1; //Devuelve nro del 1-5
+        setImg(random, rivalChoice)
+        console.log({rivalChoice})
+    }
+
+    const setImg = (option, img) =>{
+        switch (option) {
+            case 1:
+            case "Rock":
+                img.src = "/Images/Rock.png"
+                img.hidden = false
+                img.value = "Rock"
+                break;
+              
+            case 2:
+            case "Paper":
+                img.src = "/Images/Paper.png"
+                img.hidden = false
+                img.value = "Paper"
+                break;
+              
+            case 3:
+            case "Scissors":
+                img.src = "/Images/Scissors.png"
+                img.hidden = false
+                img.value = "Scissors"
+                break;
+
+            case 4:
+            case "Lizard":
+                img.src = "/Images/Lizard.png"
+                img.hidden = false
+                img.value = "Lizard"
+                break;
+
+            case 5:
+            case "Spock":
+                img.src = "/Images/Spock.png"
+                img.hidden = false
+                img.value = "Spock"
+                break;
+
+            default:
+        }
+    }
 
     const decideWinner = () => {
         
@@ -97,111 +156,16 @@ const Game = () => {
         }
     }
 
-    const chooseRival = (event) =>{
-        if(event.target.id === "Player"){
-            setVS("player")
-        } else{
-            setVS("computer") 
-        }
-        rematch()
-        document.getElementById("userPlayer").hidden = false
-        document.getElementById("changeRival").hidden = false
-        document.getElementById("vs").hidden = true
+    const playerLost = () =>{
+        if(vs === "computer"){setMatchResult("You lost this match")} 
+        else{setMatchResult("The second player win this match")}
+        setRivalScore(rivalScore + 1)
     }
 
-    const hideOptions = () =>{
-        rock.hidden = true
-        paper.hidden = true
-        scissors.hidden = true
-        lizard.hidden = true
-        spock.hidden = true
-    }
-
-    const rematch = () =>{
-        playerChoice.hidden = true
-        rivalChoice.hidden = true
-        
-        document.getElementById("userPlayer").hidden = false
-        document.getElementById("rematch").hidden = true
-        document.getElementById("result").hidden = true
-        document.getElementById("matchOptions").hidden = true
-
-        rock.hidden = false
-        paper.hidden = false
-        scissors.hidden = false
-        lizard.hidden = false
-        spock.hidden = false   
-
-    }
-
-    const setRockImg = (img) =>{
-        img.src = "/Images/Rock.png"
-        img.hidden = false
-        img.value = "Rock"
-    }
-
-    const setPaperImg = (img) =>{
-        img.src = "/Images/Paper.png"
-        img.hidden = false
-        img.value = "Paper"
-    }
-
-    const setScissorsImg = (img) =>{
-        img.src = "/Images/Scissors.png"
-        img.hidden = false
-        img.value = "Scissors"
-    }
-
-    const setLizardImg = (img) =>{
-        img.src = "/Images/Lizard.png"
-        img.hidden = false
-        img.value = "Lizard"
-    }
-
-    const setSpockImg = (img) =>{
-        img.src = "/Images/Spock.png"
-        img.hidden = false
-        img.value = "Spock"
-    }
-
-    const computerPlay = (event) => {
-
-        const random = Math.floor(Math.random() * 5) + 1; //Devuelve nro del 1-5
-        
-        switch (random) {
-            case 1:
-                setRockImg(rivalChoice)
-                break;
-              
-            case 2:
-                setPaperImg(rivalChoice)
-                break;
-              
-            case 3:
-                setScissorsImg(rivalChoice)
-                break;
-
-            case 4:
-                setLizardImg(rivalChoice)
-                break;
-
-            case 5:
-                setSpockImg(rivalChoice)
-                break;
-
-            default:
-        }
-    }
-
-    const computer = (event) =>{
-        console.log(vs)
-        playerPlay(event.target.id, playerChoice)
-        computerPlay()
-        decideWinner(playerChoice, rivalChoice)
-    }
-
-    const player = (event) => {
-        playerPlay(event.target.id, playerChoice)
+    const playerWin = () =>{
+        if(vs === "computer"){setMatchResult("You win this match")} 
+        else{setMatchResult("The first player win this match")}
+        setPlayerScore(playerScore + 1)
     }
 
     const resetScore = () =>{
@@ -209,55 +173,13 @@ const Game = () => {
         setPlayerScore(0)
     }
 
-    const playerPlay = (id, play) => {
-            document.getElementById("userPlayer").hidden = true
-
-            if(document.getElementById("rivalPlayer").hidden && vs === "player"){
-                document.getElementById("rivalPlayer").hidden = false
-            } else{
-                document.getElementById("rivalPlayer").hidden = true
-                document.getElementById("rematch").hidden = false
-                document.getElementById("result").hidden = false
-                document.getElementById("matchOptions").hidden = false
-            }
-            switch (id) {
-                case "Rock":
-                    setRockImg(play)
-                    break;
-                  
-                case "Paper":
-                    setPaperImg(play)
-                    break;
-                  
-                case "Scissors":
-                    setScissorsImg(play)
-                    break;
-    
-                case "Lizard":
-                    setLizardImg(play)
-                    break;
-    
-                case "Spock":
-                    setSpockImg(play)
-                    break;
-
-                default:
-            }
-
-            if(play === rivalChoice){
-                decideWinner(playerChoice, rivalChoice)
-            }
-    }
-
     const enableRivals = () =>{
-        
+        resetScore()
+        document.getElementById("vs").hidden = false
         document.getElementById("userPlayer").hidden = true
         document.getElementById("rivalPlayer").hidden = true
         document.getElementById("result").hidden = true
         document.getElementById("matchOptions").hidden = true
-        hideOptions()
-        resetScore()
-        document.getElementById("vs").hidden = false
         document.getElementById("changeRival").hidden = true
     }
 
@@ -273,45 +195,40 @@ const Game = () => {
                 <img className="button" id="Player" src="/Images/Player.png" value ="Player" onClick={chooseRival}/>
                 </div>
             </div>
-            {/*
-            <PlayButtons clickFuntion={eval(vs)} choice={playerChoice}/>
-            <PlayButtons clickFuntion={playerPlay} choice={rivalChoice}/>
-            */}
+            
             <div hidden id="userPlayer" className="playerButtons">Take your pick player<br/><br/>
-                <img className="button" id="Rock" src="/Images/Rock.png" value ="Rock" onClick={eval(vs)}/>
-                <img className="button" id="Paper" src="/Images/Paper.png" value ="Paper" onClick={eval(vs)}/>
-                <img className="button" id="Scissors" src="/Images/Scissors.png" value ="Scissors" onClick={eval(vs)}/>
-                <img className="button" id="Lizard" src="/Images/Lizard.png" value ="Lizard" onClick={eval(vs)}/>
-                <img className="button" id="Spock" src="/Images/Spock.png" value ="Spock" onClick={eval(vs)}/>
+                <img className="button" id="Rock" src="/Images/Rock.png" value ="Rock" onClick={(event) => eval(vs)(event, playerChoice)}/>
+                <img className="button" id="Paper" src="/Images/Paper.png" value ="Paper" onClick={(event) => eval(vs)(event, playerChoice)}/>
+                <img className="button" id="Scissors" src="/Images/Scissors.png" value ="Scissors" onClick={(event) => eval(vs)(event, playerChoice)}/>
+                <img className="button" id="Lizard" src="/Images/Lizard.png" value ="Lizard" onClick={(event) => eval(vs)(event, playerChoice)}/>
+                <img className="button" id="Spock" src="/Images/Spock.png" value ="Spock" onClick={(event) => eval(vs)(event, playerChoice)}/>
             </div>
             
             <div hidden id="rivalPlayer" className="playerButtons">Take your pick rival<br/><br/>
-                <img className="button" id="Rock" src="/Images/Rock.png" value ="Rock" onClick={() => playerPlay("Rock", rivalChoice)}/>
-                <img className="button" id="Paper" src="/Images/Paper.png" value ="Paper" onClick={() => playerPlay("Paper", rivalChoice)}/>
-                <img className="button" id="Scissors" src="/Images/Scissors.png" value ="Scissors" onClick={() => playerPlay("Scissors", rivalChoice)}/>
-                <img className="button" id="Lizard" src="/Images/Lizard.png" value ="Lizard" onClick={() => playerPlay("Lizard", rivalChoice)}/>
-                <img className="button" id="Spock" src="/Images/Spock.png" value ="Spock" onClick={() => playerPlay("Spock", rivalChoice)}/>
+                <img className="button" id="Rock" src="/Images/Rock.png" value ="Rock" onClick={(event) => player(event, rivalChoice)}/>
+                <img className="button" id="Paper" src="/Images/Paper.png" value ="Paper" onClick={(event) => player(event, rivalChoice)}/>
+                <img className="button" id="Scissors" src="/Images/Scissors.png" value ="Scissors" onClick={(event) => player(event, rivalChoice)}/>
+                <img className="button" id="Lizard" src="/Images/Lizard.png" value ="Lizard" onClick={(event) => player(event, rivalChoice)}/>
+                <img className="button" id="Spock" src="/Images/Spock.png" value ="Spock" onClick={(event) => player(event, rivalChoice)}/>
             </div>
             
             <div hidden id="result" className="match">
-                <img hidden className="img" id="playerChoice" src="" alt="playerChoice"></img>
-                <img hidden className="img" id="rivalChoice" src="" alt="rivalChoice"></img>
+                <img className="img" id="playerChoice" src="" alt="playerChoice"/>
+                <img className="img" id="rivalChoice" src="" alt="rivalChoice"/>
                 <div>
                     {matchResult}<br/>
                     Total Score
                     {playerScore} -- {rivalScore}
                 </div>
             </div>
+            
             <div hidden id="matchOptions" className="options" hidden>
                 <button id="rematch" type="button" className="btn btn-outline-dark" onClick={rematch}>Rematch!</button>
                 <button id="resetScore" type="button" className="btn btn-outline-dark" onClick={resetScore}>Reset Score</button></div>
+            
             <div hidden id="changeRival" className="options">
                 <button type="button" className="btn btn-outline-dark" onClick={enableRivals}>Change Rival</button>
             </div>
-                
-            
-            
-            
         </>
     )
 }
